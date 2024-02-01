b98aab0ae43f3836f98848d5397f7fbf320d4a39
20201102456hele <155301593+20201102456hele@users.noreply.github.com>
2024-02-01 20:32:50 +0800

    Add files via upload

--- card.ts
+++ card.ts
@@ -1,41 +1,28 @@
-import request from '@/utils/system/request'
+import { MockMethod } from 'vite-plugin-mock'
 
-// 获取数据api
-export function getData(data: object) {
-  return request({
-    url: '/card/list',
+export default [
+  {
+    url: `/mock/card/list`,
     method: 'post',
-    baseURL: '/mock',
-    data
-  })
-}
-
-// 新增
-export function add(data: object) {
-  return request({
-    url: '/card/add',
-    method: 'post',
-    baseURL: '/mock',
-    data
-  })
-}
-
-// 编辑
-export function update(data: object) {
-  return request({
-    url: '/card/update',
-    method: 'post',
-    baseURL: '/mock',
-    data
-  })
-}
-
-// 删除
-export function del(data: object) {
-  return request({
-    url: '/card/del',
-    method: 'post',
-    baseURL: '/mock',
-    data
-  })
-}
+    response: ({ body }) => {
+      const { page, pageSize } = body
+      return {
+        code: 200,
+        data: {
+          [`list|${pageSize}`]: [{
+            'id|+1': 0,
+            title: '@ctitle',
+            image: 'http://blog.51weblove.com/wp-content/uploads/2019/03/2019032323331541.jpg',
+            time: '@date(yyyy-MM-dd hh:mm:ss)'
+          }],
+          pager: {
+            page: page,
+            pageSize: pageSize,
+            total: 198
+          }
+        },
+        msg: ''
+      };
+    }
+  },
+]
--- user.ts
+++ user.ts
@@ -1,47 +1,85 @@
-import request from '@/utils/system/request'
-
-// 获取数据api
-export function getData(data: object) {
-  return request({
-    url: '/system/user/list',
+import { MockMethod } from 'vite-plugin-mock'
+const users = [
+  { name: 'admin', password: '123456', token: 'admin', info: {
+    name: '系统管理员'
+  }},
+  { name: 'editor', password: '123456', token: 'editor', info: {
+    name: '编辑人员'
+  }},
+  { name: 'test', password: '123456', token: 'test', info: {
+    name: '测试人员'
+  }},
+]
+export default [
+  {
+    url: `/mock/user/login`,
     method: 'post',
-    data
-  })
-}
-
-// 新增
-export function add(data: object) {
-  return request({
-    url: '/system/user/add',
+    response: ({ body }) => {
+      const user = users.find(user => {
+        return body.name === user.name && body.password === user.password
+      })
+      if (user) {
+        return {
+          code: 200,
+          data: {
+            token: user.token,
+          },
+        };
+      } else {
+        return {
+          code: 401,
+          data: {},
+          msg: '用户名或密码错误'
+        };
+      }
+      
+    }
+  },
+  {
+    url: `/mock/user/info`,
     method: 'post',
-    data
-  })
-}
-
-// 编辑
-export function update(data: object) {
-  return request({
-    url: '/system/user/update',
+    response: ({ body }) => {
+      const { token } = body
+      const info = users.find(user => {
+        return user.token === token
+      }).info
+      if (info) {
+        return {
+          code: 200,
+          data: {
+            info: info
+          },
+        };
+      } else {
+        return {
+          code: 403,
+          data: {},
+          msg: '无访问权限'
+        };
+      }
+      
+    }
+  },
+  {
+    url: `/mock/user/out`,
     method: 'post',
-    baseURL: '/mock',
-    data
-  })
-}
-
-// 状态变更
-export function updateStatus(data: object) {
-  return request({
-    url: '/system/user/updateStatus',
+    response: () => {
+      return {
+        code: 200,
+        data: {},
+        msg: 'success'
+      };
+    }
+  },
+  {
+    url: `/mock/user/passwordChange`,
     method: 'post',
-    baseURL: '/mock',
-    data
-  })
-}
-
-// 删除
-export function del(id: string) {
-  return request({
-    url: '/system/user/del/'+id,
-    method: 'delete',
-  })
-}
+    response: () => {
+      return {
+        code: 200,
+        data: {},
+        msg: 'success'
+      };
+    }
+  },
+] as MockMethod[]
